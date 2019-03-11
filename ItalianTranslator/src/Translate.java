17cd6ea8fea0d00f73fd2bcb9b2bcfcccf8b4043
jamesshaw <jamesshaw@labpc042.computing.dundee.ac.uk>
2019-03-11 15:26:38 +0000

    Methods for Translate class created

--- ItalianTranslator/src/Translate.java
+++ ItalianTranslator/src/Translate.java
@@ -3,9 +3,22 @@
  */
 
 /**
- * @author jamesshaw!!
+ * Class which creates translating functionality using tree class 
+ * 
+ * @author James, Josh, Jesse, Luke.
  *
  */
 public class Translate {
-
+	Tree tree = new Tree();
+	
+	public void translateText(String languageFrom, String searchText) {
+	}
+	
+	public void translatePhrase(String languageFrom, String searchPhrase) {
+		
+	}
+	
+	public void translateWord(String languageFrom, String searchWord) {
+		
+	}
 }
