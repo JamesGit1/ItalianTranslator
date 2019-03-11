e9371edbf903b510c4efcf3d0e11143c6a182f93
Luke <luke.dawe@gmail.com>
2019-03-11 16:09:04 +0000

    Added constructor methods.

--- ItalianTranslator/src/Tree.java
+++ ItalianTranslator/src/Tree.java
@@ -1,34 +1,36 @@
 
 public class Tree {
+	public Node italianRoot;
+	public Node englishRoot;
 
-	
-	
-	
-	
-	
-	
-	// Pass in the Main Binary tree into here 
-	
-	
-	
-	/**
-	 *Displays the entire dictionary
-	 *
-	 * @param english - Gets english left and right Tree
-	 * @param itatlian - Gets italian left and right Tree 
-	 */
-	public void  display(Node english, Node italian){
-		
-		display(english.getEnglishLeft(),italian.getItalianLeft());
-		System.out.println(english+ "\t" + italian);
-		display(english.getEnglishRight(),italian.getItalianRight());
-		
+	public Tree() {
+		englishRoot = null;
+		italianRoot = null;
 	}
-	
-public void loadDictionary() {
-	System.out.println("Test");
-}
-	
-	
-	
+
+	public Tree(Node italianRoot, Node englishRoot) {
+		this.englishRoot = englishRoot;
+		this.italianRoot = italianRoot;
+	}
+
+	// Pass in the Main Binary tree into here
+
+	/**
+	 * Displays the entire dictionary
+	 *
+	 * @param english  - Gets English left and right Tree
+	 * @param itatlian - Gets Italian left and right Tree
+	 */
+	public void display(Node english, Node italian) {
+
+		display(english.getEnglishLeft(), italian.getItalianLeft());
+		System.out.println(english + "\t" + italian);
+		display(english.getEnglishRight(), italian.getItalianRight());
+
+	}
+
+	public void loadDictionary() {
+		System.out.println("Test");
+	}
+
 }
