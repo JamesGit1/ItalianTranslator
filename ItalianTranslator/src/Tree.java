c9a12ba91e0b0d334b754a3ee63b5e656414d82e
Luke <luke.dawe@gmail.com>
2019-03-14 14:32:01 +0000

    Added findNode(), not tested.

--- ItalianTranslator/src/Tree.java
+++ ItalianTranslator/src/Tree.java
@@ -21,11 +21,9 @@ public class Tree {
 	 * @param itatlian - Gets Italian left and right Tree
 	 */
 	public void displayTree(Node english, Node italian) {
-
 		displayTree(english.getEnglishLeft(), italian.getItalianLeft());
 		System.out.println(english + "\t" + italian);
 		displayTree(english.getEnglishRight(), italian.getItalianRight());
-
 	}
 
 	public void loadDictionary() {
@@ -42,4 +40,28 @@ public class Tree {
 			System.out.println("File not found");
 		}
 	}
+
+	public Node findNode(String searchWord, String language) {
+		Node current;
+		if (language.equals("italian")) {
+			current = italianRoot;
+		} else {
+			current = englishRoot;
+		}
+		while (current != null) {
+			if (current.getWord(language).equals(searchWord)) {
+				return current;
+			} else if (current.getWord(language).compareTo(searchWord) < 0) {
+				current = current.getLeft(language);
+			} else {
+				current = current.getRight(language);
+			}
+		}
+		return null;
+	}
+
+	public void removeFromTree(Node nodeToDelete) {
+
+	}
+
 }
