99bf3927cf6b12cc74656a030c5642e03ca7801d
jessechan <jessechan@labpc134.computing.dundee.ac.uk>
2019-03-14 15:15:51 +0000

    Tree class

--- ItalianTranslator/src/Tree.java
+++ ItalianTranslator/src/Tree.java
@@ -1,62 +1,101 @@
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
-
 public class Tree {
+	public Node italianRoot;
+	public Node englishRoot;
 
-	
-	
-	
-	
-	
-	
-	// Pass in the Main Binary tree into here 
-	
-	
-	
-	/**
-	 *Displays the entire dictionary
-	 *
-	 * @param english - Gets english left and right Tree
-	 * @param itatlian - Gets italian left and right Tree 
-	 */
-	public void  display(Node english, Node italian){
-		
-		display(english.getEnglishLeft(),italian.getItalianLeft());
-		System.out.println(english+ "\t" + italian);
-		display(english.getEnglishRight(),italian.getItalianRight());
-		
+	public Tree() {
+		englishRoot = null;
+		italianRoot = null;
 	}
-	
-public void loadDictionary() {
-	System.out.println("Test");
-}
-	
-public void saveDictionary(Node current) {
-	
 
-try {
-		
-	if(current == null) {
-		return;
-		//yy
-}
-	
-	
-		
-		
-		
-		
-}catch(Exception e) {
-	
-	System.out.println("File not found");
-	
-}
+	public Tree(Node italianRoot, Node englishRoot) {
+		this.englishRoot = englishRoot;
+		this.italianRoot = italianRoot;
+	}
 
+	// Pass in the Main Binary tree into here
 
+	/**
+	 * Displays the entire dictionary
+	 *
+	 * @param english  - Gets English left and right Tree
+	 * @param itatlian - Gets Italian left and right Tree
+	 */
+	public void displayTree(Node english, Node italian) {
+		displayTree(english.getEnglishLeft(), italian.getItalianLeft());
+		System.out.println(english + "\t" + italian);
+		displayTree(english.getEnglishRight(), italian.getItalianRight());
+	}
 
-	
-	
-}
-	
-	
+	public Node getRoot(String language) {
+		if (language.equals("italian")) {
+			return italianRoot;
+		} else {
+			return englishRoot;
+		}
+	}
+
+	public void loadDictionary() {
+		System.out.println("Test");
+	}
+
+	public void saveDictionary(Node current) {
+		try {
+			if (current == null) {
+				return;
+				// yy
+			}
+		} catch (Exception e) {
+			System.out.println("File not found");
+		}
+	}
+
+	public Node findNode(String searchWord, String language) {
+		Node current;
+		current = getRoot(language);
+		while (current != null) {
+			if (current.getWord(language).equals(searchWord)) {
+				return current;
+			} else if (current.getWord(language).compareTo(searchWord) < 0) {
+				current = current.getLeft(language);
+			} else {
+				current = current.getRight(language);
+			}
+		}
+		return null;
+	}
+
+	public Node findParentNode(String wordToFind, String language) {
+		Node current = getRoot(language);
+		Node previous = null;
+		while (current != null) {
+			if (current.getWord(language).equals(wordToFind)) {
+				return previous;
+			} else if (wordToFind.compareTo(current.getWord(language)) < 0) {
+				previous = current;
+				current = current.getRight(language);
+			} else {
+				previous = current;
+				current = current.getLeft(language);
+			}
+		}
+		return null;
+	}
+
+	public void removeFromTree(String wordToDelete, String language) {
+		Node nodeToDelete = findNode(wordToDelete, language);
+		Node parentNode;
+	}
+
+	/**
+	 * This method checks if the tree is empty.
+	 * 
+	 * @return true or false
+	 */
+	public boolean isTreeEmpty() {
+		if (englishRoot == null) {
+			return true;
+		}
+		return false;
+
+	}
 }
