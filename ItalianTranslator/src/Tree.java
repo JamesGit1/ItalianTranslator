863e82e97d3c02d8ca46f25d623043d6514cd751
jessechan <jessechan@labpc134.computing.dundee.ac.uk>
2019-03-14 16:14:46 +0000

    Fixed Dislay  and Save added Load

--- ItalianTranslator/src/Tree.java
+++ ItalianTranslator/src/Tree.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Tree {
 	public Node italianRoot;
 	public Node englishRoot;
@@ -20,11 +26,7 @@ public class Tree {
 	 * @param english  - Gets English left and right Tree
 	 * @param itatlian - Gets Italian left and right Tree
 	 */
-	public void displayTree(Node english, Node italian) {
-		displayTree(english.getEnglishLeft(), italian.getItalianLeft());
-		System.out.println(english + "\t" + italian);
-		displayTree(english.getEnglishRight(), italian.getItalianRight());
-	}
+
 
 	public String changeLanguage(String language) {
 		if (language.equals("italian")) {
@@ -42,19 +44,66 @@ public class Tree {
 		}
 	}
 
+	public void displayTree(Node current) {
+
+		displayTree(current.getEnglishLeft());
+		System.out.println(current.getEnglishTranslation() + "\t" + current.getItalianTranslation());
+		displayTree(current.getEnglishRight());
+
+	}
+
 	public void loadDictionary() {
-		System.out.println("Test");
+		FileReader fr = null;
+		BufferedReader br = null;
+		try {
+			fr = new FileReader("A.txt");
+			br = new BufferedReader(fr);
+
+			String nextLine = br.readLine();
+
+			while (nextLine != null) {
+				String[] words = nextLine.split(",");
+
+				// Add to tree pass word 0 and 1
+				nextLine = br.readLine();
+			}
+
+			br.close();
+
+		} catch (Exception e) {
+
+			System.out.println("heehsheshes");
+
+		}
 	}
 
 	public void saveDictionary(Node current) {
+
+		FileOutputStream os = null;
+		PrintWriter pw = null;
+
 		try {
-			if (current == null) {
-				return;
-				// yy
-			}
-		} catch (Exception e) {
+			os = new FileOutputStream("Dictionary.txt");
+			pw = new PrintWriter(os);
+
+			save(current, pw);
+			pw.flush();
+
+		} catch (IOException e) {
 			System.out.println("File not found");
+
 		}
+	}
+
+	public void save(Node current, PrintWriter pw) {
+
+		if (current != null) {
+			save(current.getEnglishLeft(), pw);
+			pw.print(current.getEnglishTranslation() + "," + current.getItalianTranslation());
+			save(current.getEnglishRight(), pw);
+
+		}
+
 	}
 
 	public Node findNode(String searchWord, String language) {
